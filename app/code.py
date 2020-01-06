573a502aecd2dd2028bffc5e2871124a62793367
Milena <milenavdvelde@gmail.com>
2020-01-06 13:25:25 +0100

    pseudocode

--- app/code.py
+++ app/code.py
@@ -1,3 +1,15 @@
 '''
 pseudocode
 '''
+
+Class Trains():
+    def __init__(self,stops,total):
+        self.p = stops/total
+    
+    def traject(self,number):
+        # traject van a naar b, met mogelijke tussenstops en totale tijd
+        # minimaliseren van functie K= p*10000-(T*100+Min)
+        name_traject = number
+        stops = {}
+        time = int
+        
