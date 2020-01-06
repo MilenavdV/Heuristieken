a1afaa9875e8e8e1b96cf62435b5e0161bb50eaa
Wouter de Boer <MRFARMER@wcw-stud-145-109-15-216.wireless.uva.nl>
2020-01-06 13:50:12 +0100

    stations.py

---
+++ code/readstations.py
@@ -0,0 +1,10 @@
+import csv
+
+stations = {}
+
+with open('StationsNationaal.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        stations[row[0]] = [row[1], row[2]]
+
+print(stations)
