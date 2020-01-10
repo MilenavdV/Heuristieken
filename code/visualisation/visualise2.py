3d70814f2d47fdf863127c8b1cbb1b65be4eca99
Milena <milenavdvelde@gmail.com>
2020-01-10 15:41:48 +0100

    traject toegevoegd

--- code/visualisation/visualise2.py
+++ code/visualisation/visualise2.py
@@ -11,7 +11,7 @@ from bokeh.models import HoverTool
 output_file("geojson.html")
 map_options = GMapOptions(lat=52, lng=5, map_type="roadmap", zoom=7)
 
-# p = gmap("AIzaSyCnGx0iTuaHmLPA8LdqDnIo7vK15mp5sww", map_options)
+p = gmap("AIzaSyCnGx0iTuaHmLPA8LdqDnIo7vK15mp5sww", map_options)
 
 # data = json.loads("data/StationsNationaal.json")
 with open("data/StationsNationaal2.json", 'r') as geo_file:
@@ -27,9 +27,21 @@ TOOLTIPS = [
     ('Station', '@name')
 ]
 
-p = figure(background_fill_color="lightgrey", tooltips=TOOLTIPS)
+x1 = []
+y1 = []
+with open("data/trajectcoordinaten2.csv", mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    for row in csv_reader:
+        x1.append([float(row[2]),float(row[4])])
+        # x1.append(float(row[4]))
+        y1.append([float(row[3]),float(row[5])])
+        
 
-# p.line(x='x', y='y',source=geo_source)
+# print(y1,x1)
+# print(y1)
+# p = figure(background_fill_color="lightgrey", tooltips=TOOLTIPS)
+
+p.multi_line(y1,x1,line_width=2)
 p.circle(x='x', y='y', size=5, color='Color', alpha=0.7, source=geo_source)
 show(p)
 
