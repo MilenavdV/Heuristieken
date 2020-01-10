48396f1ab723402fc7eaadc3c2ec8fc04b41831a
Milena <milenavdvelde@gmail.com>
2020-01-10 14:24:57 +0100

    kaartje

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -1,49 +1,39 @@
-import json
+import math 
+import csv
 
-from bokeh.io import show
-from bokeh.models import GeoJSONDataSource
+from bokeh.io import show, output_file
 from bokeh.plotting import figure
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+
+plot = figure(title='Graph Layout Demonstration', x_range=(51,54), y_range=(4,7),
+              tools='', toolbar_location=None)
+
+graph = GraphRenderer()
+node_indices = list(range(28))
+graph.node_renderer.data_source.add(node_indices, 'index')
+graph.node_renderer.data_source.add(Spectral8, 'color')
+graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+
+graph.edge_renderer.data_source.data = dict(
+    start=[0]*28,
+    end=node_indices)
 
 
-def visualise(graph, geo_file):
-    """
-    Visualisation code that uses bokeh and geometry data from a JSON file
-    to represent a coloured graph.
-    """
 
-    with open(geo_file, 'r') as geo_file:
-        data = json.load(geo_file)
+x = []
+y = []
+with open('data/StationsNationaal.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        x.append(float(row[1]))
+        y.append(float(row[2]))
 
-    # Dict comprehension that creates a dictionary with a lowercase name for the
-    # node if it has at least one neighbour
-    states = {node.name.lower(): node for node in graph.nodes.values() if len(node.neighbours) != 0}
+graph_layout = dict(zip(node_indices,zip(x, y)))
+print(graph_layout)
+graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-    # Take the features of the states that have at least one neighbour
-    for i in range(len(data['features'])):
-        data['features'] = [feature for feature in data['features'] if
-                            feature['properties']['NAME'].lower().replace(' ', '') in states]
+plot.renderers.append(graph)
 
-    # Get the colour of the states from the corresponding nodes
-    for feature in data['features']:
-        feature['properties']['colour'] = states[
-            feature['properties']['NAME'].lower().replace(' ', '')].get_value().colour.get_web()
-        feature['properties']['cost'] = states[
-            feature['properties']['NAME'].lower().replace(' ', '')].get_value().value
-        feature['properties']['transmitter'] = states[
-            feature['properties']['NAME'].lower().replace(' ', '')].get_value().name
-
-    geo_source = GeoJSONDataSource(geojson=json.dumps(data))
-
-    # Set the bokeh tooltips
-    TOOLTIPS = [
-        ("(x,y)", "($x, $y)"),
-        ("State", "@NAME"),
-        ("Transmitter", "@transmitter"),
-        ("Cost", "@cost")
-    ]
-
-    p = figure(background_fill_color="lightgrey", tooltips=TOOLTIPS)
-    p.sizing_mode = 'scale_height'
-    p.patches(xs='xs', ys='ys', fill_color='colour', line_color='black', line_width=0.2, source=geo_source)
-
-    show(p)
+output_file('graph.html')
+show(plot)
