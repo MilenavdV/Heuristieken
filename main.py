5d2c66ca040b6ae481019b3eb1de7ad07ed5bd77
Milena <milenavdvelde@gmail.com>
2020-01-13 13:55:10 +0100

    visualisatie

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-from code.algorithms.randomize import randomnize
-=======
 from code.algorithms.randomize import randomize
->>>>>>> e851b77c20a7e5fb7674f93291f5309d0f47666c
 
 if __name__ == "__main__":
     randomize('data/ConnectiesHolland.csv')
