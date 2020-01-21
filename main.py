01607a85c9c19f90153aa41d397c132595d20e09
Milena <milenavdvelde@gmail.com>
2020-01-21 16:41:58 +0100

    main.py aanpassingen

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# from code.algorithms.readconnections import readConnections
-# from code.algorithms.fastestoption import fastestOption
-# from code.algorithms.csvwriter import csvWriter
-# from code.algorithms.kruskal import kruskal
-# from code.algorithms.lookahead_climber import lookaheadClimber
-# from code.algorithms.oldrandomize import oldrandomize
-# from code.algorithms.connectioncount import Count
-# from code.algorithms.hillclimb import HillClimber
-# from code.visualisation.visualise import visualise
-# from code.visualisation.boxplot import Vergelijking
 from code.algorithms.connectioncount import Count
 from code.algorithms.lookahead_climber import Lookahead
 from code.algorithms.kruskal import Kruskal
 from code.algorithms.oldrandomize import Randomize
+from code.algorithms.hillclimb import HillClimber
+
 import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -25,8 +17,9 @@ if __name__ == "__main__":
     station_file = 'data/StationsNationaal.csv'
     file = 'data/ConnectiesNationaal.csv'
     output = 'dienstregeling.csv'
-    # stations, cdict, clist, clist2 = readConnections(file)
     plt.close('all')
+
+
 
     test = Randomize(file,trains,timeframe)
     trajecten,p,score,train_used = test.randomize()
