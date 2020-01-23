401b86544c7e57d8d24d261a5659a266495acd70
Wouter de Boer <wouter.de.boer1998@gmail.com>
2020-01-23 14:54:51 +0100

    Comments

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -6,69 +6,6 @@ import json
 import csv
 import random
 
-<<<<<<< HEAD
-def visualise(train_used):
-    output_file("geojson.html")
-    # map_options = GMapOptions(lat=52.5, lng=5, map_type="roadmap", zoom=7)
-
-    # p = gmap("AIzaSyCnGx0iTuaHmLPA8LdqDnIo7vK15mp5sww", map_options)
-
-    with open("data/StationsNationaal2.json", 'r') as geo_file:
-            data = json.load(geo_file)
-    stations =[]
-    for i in range(len(data['features'])):
-        data['features'][i]['properties']['Color'] = ['black', 'black'][i%2]
-        stations.append(data['features'][i]['properties']['name'])
-    # source = ColumnDataSource(names=stations)
-    geo_source = GeoJSONDataSource(geojson=json.dumps(data))
-    # print(stations)
-    TOOLTIPS = [
-        ('Station', '@name')
-    ]
-
-    x1 = []
-    y1 = []
-    with open("data/trajectcoordinaten2.csv", mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=';')
-        for row in csv_reader:
-            y1.append([float(row[2]),float(row[4])])
-            x1.append([float(row[3]),float(row[5])])
-            
-    p = figure(background_fill_color="lightgrey", plot_width=800,plot_height=800, tools="tap")
-    p.multi_line(x1,y1,line_width=1)
-
-    x2 = []
-    y2 = []
-    i = 0
-    trajecten_x ={}
-    trajecten_y = {}
-    with open("dienstregeling.csv", mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')        
-        for row in csv_reader:
-            traject = 'Traject ' + str(i + 1)
-            if row == []:
-                continue
-            if 'Total' in row[0]:
-                continue
-            if row[0] == traject:
-                i = i+1
-                trajecten_y[i] = []
-                trajecten_x[i] = []
-                continue
-            trajecten_y[i].append(row[2])
-            trajecten_y[i].append(row[4])
-            trajecten_x[i].append(row[3])
-            trajecten_x[i].append(row[5])
-
-    for i in range(train_used):
-        r = random.randint(0,255)
-        g = random.randint(0,255)
-        b = random.randint(0,255)
-        #
-        p.line(trajecten_x[i+1],trajecten_y[i+1],line_width=2,color=(r,g,b))
-        
-    show(p)
-=======
 class Visualise:
     def __init__(self,train_used):
         self.train_used = train_used
@@ -111,7 +48,7 @@ class Visualise:
         i = 0
         trajecten_x ={}
         trajecten_y = {}
-        with open("dienstregeling2-1.csv", mode='r') as csv_file:
+        with open("dienstregeling2.csv", mode='r') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')        
             for row in csv_reader:
                 traject = 'Traject ' + str(i + 1)
@@ -141,5 +78,4 @@ class Visualise:
         
         # open the map
         show(p)
->>>>>>> b96cdd9d485cf98e65a9c05213e6aaac9953cd64
 
--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-from code.algorithms.readconnections import readConnections
-from code.algorithms.fastestoption import fastestOption
-from code.algorithms.csvwriter import csvWriter
-from code.algorithms.kruskal import kruskal
-from code.algorithms.royspartytent import randomize
-from code.algorithms.vrijdag import test
-from code.algorithms.hillclimb import HillClimber
-from code.visualisation.visualise import visualise
-from code.algorithms.csvreader import checkScore
-=======
 from code.algorithms.greedy import Greedy
 from code.algorithms.csvwriter import csvWriter
 from code.algorithms.kruskal import Kruskal
@@ -19,12 +8,15 @@ from code.algorithms.hillclimb import HillClimber
 # from code.visualisation.boxplot import Vergelijking
 from code.algorithms.connectioncount import Count
 from code.visualisation.visualise import Visualise
+from code.algorithms.royspartytent import randomize
+from code.algorithms.readconnections import readConnections
+from code.algorithms.functions import *
+from code.algorithms.csvreader import *
 
 import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
 
->>>>>>> b96cdd9d485cf98e65a9c05213e6aaac9953cd64
 
 if __name__ == "__main__":
     # # # sims = 10
@@ -32,7 +24,6 @@ if __name__ == "__main__":
     timeframe = 180
     station_file = 'data/StationsNationaal.csv'
     file = 'data/ConnectiesNationaal.csv'
-<<<<<<< HEAD
     output = 'dienstregeling.csv'
     stations, cdict, clist, clist2 = readConnections(file)
     time = 180
@@ -41,37 +32,23 @@ if __name__ == "__main__":
     while True:
 
         trajecten,scorerandom,p,trains_used = randomize(cdict, clist, trains, timeframe)
-        #print(scorerandom)
         if scorerandom > high:
             print(scorerandom, ">", high)
             high = scorerandom
             
-        if scorerandom > 7143 and scorerandom == high:
-            csvWriter('dienstregeling2.csv',trajecten)
+        if scorerandom > 7186 and scorerandom == high:
+            csvWriter(station_file, 'dienstregeling2.csv', trajecten)
             print("update csv")
 
+
+    #print(best('Zutphen', 180, cdict, [], ['Apeldoorn-Zutphen', 'Zutphen-Apeldoorn']))
   
 
     
     #csvWriter('dienstregeling.csv',trajecten)
     # result = 'dienstregeling2.csv'
     # print(checkScore(result, clist))
-
-    # visualise(train_used)
     
-=======
-    output = 'dienstregeling3.csv'
-    functie = Lookahead(file,trains,timeframe)
-    # while True:
-    #     trajecten, p, score, trains_used = functie.lookaheadClimber()
-    #     print(score,p*178)
-    #     if score >7000 or p ==1:
-    #         print('YAS')
-    #         csvWriter(station_file,f"dienstregeling{round(score)}T.csv",trajecten)
-
-    # # csvWriter(station_file,output,trajecten)
-    vis = Visualise(10)  
-    vis.map()
-
-
->>>>>>> b96cdd9d485cf98e65a9c05213e6aaac9953cd64
+    # vis = Visualise(10)
+    # vis.map()
+    
