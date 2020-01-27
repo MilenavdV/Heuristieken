86e0bb288ec9b034c1dff33e7b8327ffc702f53a
Milena <milenavdvelde@gmail.com>
2020-01-27 09:57:12 +0100

    main

--- main.py
+++ main.py
@@ -20,19 +20,40 @@ if __name__ == "__main__":
             + 'Where k is the quality, p is the proportions of all available connections covered, T amount of train used '\
             + 'and finally the MIN stands for the sum of all the connections made in the solution.'
 
-    while True:
-        print(summary)
-        station_file =input('Which file do you want to use for all the stations: \n')
-        file = input('Which file do you want to use for all the connections: \n')
-        trains = int(input('How many trains, max, do you want to use? \n'))
-        timeframe =  int(input('How many minutes is a train allowed to ride? \n'))
-        visualise = input('Do you want the answer to be visualised?')
+    
+    print(summary,"\n")
 
-        while visualise.lower() != 'no' or visualise.lower() != 'yes':
-            print('please answer with a simple yes or no')
-            visualise = input('') 
-            if visualise.lower() == 'yes' or visualise.lower() == 'no':
-                break
+    station_files = ['data/stationsHolland.csv','data/stationsNationaal.csv']
+    i = 0
+    print('Which file do you want to use for all the stations:')
+    for option in station_files:
+        print(i,':',option)
+        i += 1
+    file_options = int(input('Choose the number\n'))
+
+    station_file = station_files[file_options]
+
+    connection_files = ['data/connectiesHolland.csv','data/connectiesNationaal.csv']
+    print('Which file do you want to use for all the connections:')
+    i = 0
+    for option in connection_files:
+        print(i,':',option)
+        i += 1
+    connection_file = int(input('Choose the number\n'))
+    file = connection_files[connection_file]
+
+
+    trains = int(input('How many trains, max, do you want to use? \n'))
+    timeframe =  int(input('How many minutes is a train allowed to ride? \n'))
+    visualise = input('Do you want the answer to be visualised? \n')
+    	
+    while visualise.lower() != 'no' or visualise.lower() != 'yes':
+        print('please answer with a simple yes or no')
+        visualise = input('') 
+        if visualise.lower() == 'yes' or visualise.lower() == 'no':
+            break
+
+    while True:            
         options = ['Random', 'Greedy', 'Kruskal', 'ConnectionCount', 'hillClimber', 'Lookahead']
         print('possible algorithms:')
         i = 0
@@ -59,7 +80,60 @@ if __name__ == "__main__":
             sims = int(input('How many simulations do you want?'))
             alg = Greedy(file,trains,timeframe)
             for i in range(0,sims):
-                trajecten,p,score,train_used = alg.run()
+                trajecten, p,total_minutes,score= alg.run()
                 print('Using the Greedy algorithm the following score is found:',score)
-                print('With a p of: ',p,f'and {train_used} trains are used')
-            break
+                print('With a p of: ',p)
+            again = input('Do you want to try a different algorithm?')
+            if again.lower() == 'yes':
+                pass
+            else:
+                break
+
+        if algorithm == 2:
+            alg = Kruskal(file,trains,timeframe)
+            trajecten, p,total_minutes,score= alg.kruskal()
+            print('Using the Kruskal algorithm the following score is found:',score)
+            print('With a p of: ',p)
+            again = input('Do you want to try a different algorithm?')
+            if again.lower() == 'yes':
+                pass
+            else:
+                break
+
+        if algorithm == 3:
+            alg = Count(file,trains,timeframe)
+            trajecten, p, score, train_used= alg.connectionCount()
+            print('Using the connectioncount algorithm the following score is found:',score)
+            print('With a p of: ',p, f'and {train_used} are used.')
+            again = input('Do you want to try a different algorithm?')
+            if again.lower() == 'yes':
+                pass
+            else:
+                break
+
+        # if algorithm == 4:
+        #     sims = int(input('How many simulations do you want?'))
+        #     alg = HillClimber(file,trains,timeframe)
+        #     for i in range(0,sims):
+        #         score = alg.hillclimb()
+        #     again = input('Do you want to try a different algorithm?')
+        #     if again.lower() == 'yes':
+        #         pass
+        #     else:
+        #         break
+
+        if algorithm == 5:
+            sims = int(input('How many simulations do you want?'))
+            alg = Lookahead(file,trains,timeframe)
+            for i in range(0,sims):
+                trajecten, p, score, train_used= alg.lookaheadClimber()
+                print('Using the lookahead algorithm the following score is found:',score)
+                print('With a p of: ',p, f'and {train_used} trains are used.')
+            again = input('Do you want to try a different algorithm?')
+            if again.lower() == 'yes':
+                pass
+            else:
+                break
+
+
+
