96cdb39dda591c6ecce82f7e7dabbd4079843409
flip1998 <54834986+flip1998@users.noreply.github.com>
2020-01-28 15:21:01 +0100

    classes gecomment

--- code/classes/traject.py
+++ code/classes/traject.py
@@ -1,12 +1,13 @@
 class Traject:
+    """ a traject is one list of continuous connections and stops, 
+        representing one train line """
     def __init__(self):
         self.connections = []
         self.stops = []
         self.time = 0
 
     def addConnection(self, connection, time, timeframe):
-        # traject van a naar b, met mogelijke tussenstops en totale tijd
-        # minimaliseren van functie K= p*10000-(T*100+Min)
+        # voegt connectie toe aan traject wanneer tijd nog niet vol
         if (self.time + connection.time) <= timeframe:
             self.connections.append(connection)
             self.time += connection.time
@@ -14,11 +15,13 @@ class Traject:
         return self.stops
     
     def __str__(self):
+        # add final stop to traject
         self.stops.append(self.connections[-1].destination)
         print("")
-        print(f"Traject langs {self.stops}")
-        print("")
+        str = f"Traject langs {self.stops}"
+        # adds every connection to string
         for i in self.connections: 
-            print(i)
-        print("")
-        return f"Totale reistijd van {self.time} minuten"
+            str += i
+        # adds travel time
+        str += f"\n Totale reistijd van {self.time} minuten"
+        return str
